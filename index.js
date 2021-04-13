0609768f61f57b3042585227b380f0d678cb8a1d
wallhera <amy.wallhermfechtel@wwt.com>
2021-04-13 14:02:48 -0500

    continued improvements

--- index.js
+++ index.js
@@ -3,7 +3,6 @@ var path = require('path');
 var logger = require('morgan');
 var bodyParser = require('body-parser');
 var cors = require('cors');
-var readline = require('readline');
 var axios = require('axios');
 
 var api = express();
@@ -13,21 +12,60 @@ api.use(logger('dev'));
 api.use(bodyParser.json());
 api.use(bodyParser.urlencoded({ extended: false }));
 
-const make = "VMRDS"
-const man = "man"
-const model = "NGB mounted RN"
+const serialNumber = "VMRDS"
+const man = "NIWC"
+const model = "NGB Mounted RN Sensor"
+
+const statuses = ["STATUS_GREEN", "STATUS_YELLOW", "STATUS_RED"]
+const messages = ['super green', 'mildly out of sorts', 'AAAAACKK!']
+
 const pairs = {
-    "neutron_health_status" : "STATUS_YELLOW",
-    "neutron_health_message": "yellow message",
-    "rr_health_status" : "STATUS_RED",
-    "rr_health_message": "red message",
+    "neutron_health_status" : statuses,
+    "neutron_health_message": messages,
+    "fl_health_status" : statuses,
+    "fl_health_message": messages,
+    "fr_health_status" : statuses,
+    "fr_health_message": messages,
+    "rl_health_status" : statuses,
+    "rl_health_message": messages,
+    "rr_health_status" : statuses,
+    "rr_health_message": messages,
+    "boss_health_status" : statuses,
+    "boss_health_message": messages,
+    "neutron_serial_number": "123",
+    "fl_serialNumber" : "234",
+    "fr_serialNumber" : "345",
+    "rl_serialNumber" : "456",
+    "rr_serialNumber" : "567"
 }
 
-let id = `make=${make}&man=${man}&model=${model}&`
-let queryParams =
-Object.entries(pairs).reduce((curr, [key, value]) => 
-    curr.concat(`&${key}=${value}`)
-, "")
-console.log(queryParams)
-let url = `http://localhost:8080/sensors?${id}${queryParams}`;
-axios.get(url);
+let id = `manufacturer=${man}&model=${model}&serialNumber=${serialNumber}&`
+
+async function go() {
+let counter = 0
+while(counter < 100) {
+        let num = counter % 3
+        let queryParams = Object.entries(pairs)
+            .reduce((curr, [key, value]) => {
+                if(typeof value === 'string') {
+                    return curr.concat(`&${key}=${value}`)
+                }
+                return curr.concat(`&${key}=${value[num]}`)
+            }, "")
+
+        await sleep(1000)
+        console.log("in then " + counter)
+            
+            let url = `http://localhost:8080/sensor?${id}${queryParams}`;
+            console.log(url)
+            axios.get(url);
+            counter ++
+        
+        }
+}
+
+function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms))
+}
+
+go()
